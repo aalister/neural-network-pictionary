2b0a2aa20d8354d4847cf95fb975ed6fd7001e26
Joshua <80783216+Joshuii@users.noreply.github.com>
2022-04-02 17:56:59 +0100

    CNN with full dataset
    A working CNN that uses the entire Google draw dataset

--- CNN/main.py
+++ CNN/main.py
@@ -3,7 +3,7 @@ import os
 
 from matplotlib import pyplot as plt
 import numpy as np
-from keras import layers, models, losses
+from keras import layers, models, losses, metrics
 
 # Input & output size definitions
 INPUT_SIZE = (28, 28, 1)
@@ -18,11 +18,11 @@ def load_data():
     input_data = np.empty([0, INPUT_SIZE[0], INPUT_SIZE[1], INPUT_SIZE[2]])
     labels = np.empty([0, OUTPUT_SIZE])
 
-    all_files = glob.glob(os.path.join("res/" "*.npy"))
+    all_files = glob.glob(os.path.join("D:/Google_Draw_Dataset/" "*.npy"))
     print(all_files)
     for i, file in enumerate(all_files):
-        data = np.load(file)
-        data = np.array([d.reshape(INPUT_SIZE) for d in data[0: 2000, :]])
+        data = np.load(file, mmap_mode="r")
+        data = np.array([d.reshape(INPUT_SIZE) for d in data[4000: 4500, :]])
         # print(type(data))
         one_hot_encoding = make_one_hot_encoding(OUTPUT_SIZE, i)
         label = [one_hot_encoding for _ in range(len(data))]
@@ -59,30 +59,36 @@ def load_data():
 
 
 def main():
-    model = models.Sequential()
-
-    # Input layer
-    model.add(layers.Input(shape=INPUT_SIZE))
-
-    # Convolutional layers
-    model.add(layers.Conv2D(filters=64, kernel_size=(3, 3), activation="relu"))
-    model.add(layers.MaxPool2D(pool_size=(2, 2)))
-    model.add(layers.Conv2D(filters=256, kernel_size=(3, 3), activation="relu"))
-    model.add(layers.MaxPool2D(pool_size=(2, 2)))
-    model.add(layers.Conv2D(filters=512, kernel_size=(3, 3), activation="relu"))
-    model.add(layers.MaxPool2D(pool_size=(2, 2)))
-
-    # Fully connected layers
-    model.add(layers.Flatten())
-    model.add(layers.Dense(400, activation="relu"))
-    model.add(layers.Dense(OUTPUT_SIZE, activation="softmax"))
-
-    model.summary()
-    model.compile(
-        optimizer='adam',
-        loss="categorical_crossentropy",#losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=['accuracy']
-    )
+    model = models.load_model("anywhere2")
+    # model = models.Sequential()
+    #
+    # # Input layer
+    # model.add(layers.Input(shape=INPUT_SIZE))
+    #
+    # # Convolutional layers
+    # model.add(layers.Conv2D(filters=32, kernel_size=(3, 3), activation="relu"))  # 64
+    # model.add(layers.MaxPool2D(pool_size=(2, 2)))
+    # model.add(layers.Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))  # 256
+    # model.add(layers.MaxPool2D(pool_size=(2, 2)))
+    # model.add(layers.Conv2D(filters=360, kernel_size=(3, 3), activation="relu"))  # 512
+    # model.add(layers.MaxPool2D(pool_size=(2, 2)))
+    #
+    # # Fully connected layers
+    # model.add(layers.Flatten())
+    # model.add(layers.Dense(360, activation="relu"))
+    # model.add(layers.Dense(OUTPUT_SIZE, activation="softmax"))
+    #
+    # model.summary()
+    # # model.compile(
+    # #     loss="categorical_crossentropy",
+    # #     optimizer="adam",
+    # #     metrics=["top_k_categorical_accuracy"]
+    # # )
+    # model.compile(
+    #     optimizer='adam',
+    #     loss="categorical_crossentropy",  # losses.SparseCategoricalCrossentropy(from_logits=True),
+    #     metrics=['accuracy', metrics.TopKCategoricalAccuracy(k=5)]
+    # )
 
     test_data, test_labels, train_data, train_labels = load_data()
     print(f"test_data: {test_data.shape}")
@@ -90,13 +96,14 @@ def main():
     print(f"train_data: {train_data.shape}")
     print(f"train_labels: {train_labels.shape}")
 
-    model.fit(x=train_data, y=train_labels, validation_split=0.1, batch_size=256, verbose=2, epochs=5)
-    score = model.evaluate(train_data, train_labels, verbose=2)
-    print('Test accuracy: {:0.2f}%'.format(score[1] * 100))
+    # model.fit(x=train_data, y=train_labels, validation_split=0.1, batch_size=256, verbose=2, epochs=5)
+    # score = model.evaluate(train_data, train_labels, verbose=2)
+    # print('Test accuracy: {:0.2f}%'.format(score[1] * 100))
 
     history = model.fit(
         train_data,
         train_labels,
+        batch_size=256,
         epochs=5,
         validation_data=(test_data, test_labels)
     )
@@ -105,14 +112,32 @@ def main():
     plt.plot(history.history['val_accuracy'], label='val_accuracy')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
-    plt.ylim([0.5, 1])
     plt.legend(loc='lower right')
     plt.show()
 
-    test_loss, test_acc = model.evaluate(test_data, test_labels, verbose=2)
-    print(f"test loss: {test_loss}")
-    print(f"test acc: {test_acc}")
+    loss, acc, kacc = model.evaluate(test_data, test_labels)
+    print(f"loss: {loss}")
+    print(f"acc: {acc}")
+    print(f"kacc: {kacc}")
+
+    model.save("anywhere4")
+
+
+def test_model():
+    model = models.load_model("anywhere")
+    model.compile(
+        optimizer='adam',
+        loss="categorical_crossentropy",  # losses.SparseCategoricalCrossentropy(from_logits=True),
+        # metrics=['accuracy', "top_k_categorical_accuracy"]
+        metrics=["accuracy", metrics.TopKCategoricalAccuracy(k=10)]
+    )
+    test_data, test_labels, train_data, train_labels = load_data()
+    loss, acc, kacc = model.evaluate(test_data, test_labels)
+    print(f"loss: {loss}")
+    print(f"acc: {acc}")
+    print(f"kacc: {kacc}")
 
 
 if __name__ == "__main__":
     main()
+    # test_model()
