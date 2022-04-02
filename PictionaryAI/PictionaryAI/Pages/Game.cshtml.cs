c87f86783e7e9b8cd75740103f327f436ebb8c74
Theo <32236823+piggeywig2000@users.noreply.github.com>
2022-04-02 12:48:34 +0100

    added game page

---
+++ PictionaryAI/PictionaryAI/Pages/Game.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PictionaryAI.Pages
+{
+    public class GameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
